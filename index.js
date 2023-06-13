b5c837aba7226069cdf7c09f8debfc458f420796
Tran Dan <tergitran@gmail.com>
2023-06-13 17:36:35 +0700

    init app

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://tergitran:mylove123@cluster0.vzdi4pi.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl).then(() => {
+  console.log('connected to database');
+}).catch(error => {
+  console.log("error connecting to MongoDB:",error.message);
+});
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
